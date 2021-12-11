17372f902d6f032195ee10b4e98d2deb0d457ffd
NguyenQuang0504 <quang10a30504@gmail.com>
2021-12-11 17:29:56 +0700

    up bt

--- module3/case_study_database/quan_ly_khu_nghi_duong_furama.sql
+++ module3/case_study_database/quan_ly_khu_nghi_duong_furama.sql
@@ -216,5 +216,57 @@ left join dich_vu on dich_vu.ma_dv = hop_dong.ma_dv
 left join hop_dong_chi_tiet on hop_dong.ma_hd = hop_dong_chi_tiet.ma_hd
 left join dich_vu_di_kem on hop_dong_chi_tiet.ma_dvdk = dich_vu_di_kem.ma_dvdk;
 
--- 6.	Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+-- 6.Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
 -- của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+select dich_vu.ma_dv, dich_vu.ten_dv,dich_vu.dien_tich, dich_vu.chi_phi, loai_dich_vu.ten_ldv from dich_vu 
+left join hop_dong on hop_dong.ma_dv = dich_vu.ma_dv left join loai_dich_vu on loai_dich_vu.ma_ldv = dich_vu.ma_ldv
+where dich_vu.ma_dv not in(
+	select dich_vu.ma_dv from dich_vu 
+	left join hop_dong on hop_dong.ma_dv = dich_vu.ma_dv left join loai_dich_vu on loai_dich_vu.ma_ldv = dich_vu.ma_ldv
+	where hop_dong.ngay_lam like "2021-03-%" or hop_dong.ngay_lam like "2021-02-%" or hop_dong.ngay_lam like "2021-01-%"
+)
+ group by dich_vu.ma_dv;
+
+-- 7.Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu 
+-- của tất cả các loại dịch vụ đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.
+
+select dich_vu.ma_dv, dich_vu.ten_dv, dich_vu.dien_tich, dich_vu.so_nguoi, dich_vu.chi_phi, loai_dich_vu.ten_ldv, loai_dich_vu.ma_ldv from dich_vu
+inner join loai_dich_vu on dich_vu.ma_ldv = loai_dich_vu.ma_ldv inner join hop_dong on hop_dong.ma_dv = dich_vu.ma_dv where hop_dong.ngay_lam like "2020-%"
+and dich_vu.ma_dv not in
+(
+	select dich_vu.ma_dv from dich_vu
+	inner join loai_dich_vu on dich_vu.ma_ldv = loai_dich_vu.ma_ldv 
+	inner join hop_dong on hop_dong.ma_dv = dich_vu.ma_dv where hop_dong.ngay_lam like "2021-%"
+    group by dich_vu.ma_dv
+)
+group by dich_vu.ma_dv;
+
+-- 8.Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+select khach_hang.ten_kh from khach_hang group by ten_kh having count(ten_kh)<2;
+
+-- 9.Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+select month(hop_dong.ngay_lam) as month, count(month(hop_dong.ngay_lam)) as so_khach from khach_hang 
+right join hop_dong on hop_dong.ma_kh = khach_hang.ma_kh where hop_dong.ngay_lam like "2021%" 
+group by `month`
+order by `month`;
+    
+-- 10.	Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. 
+-- Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc,
+-- tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+
+select hop_dong.ma_hd, hop_dong.ngay_lam, hop_dong.ngay_kt, hop_dong.tien_dat, sum(hop_dong_chi_tiet.so_luong) as num_dvdk from
+hop_dong left join hop_dong_chi_tiet on hop_dong.ma_hd = hop_dong_chi_tiet.ma_hd left join
+dich_vu_di_kem on dich_vu_di_kem.ma_dvdk = hop_dong_chi_tiet.ma_dvdk group by hop_dong.ma_hd;
+
+-- 11.Hiển thị thông tin các dịch vụ đi kèm đã được sử dụng bởi những khách hàng có ten_loai_khach là “Diamond” và có dia_chi ở “Vinh” hoặc “Quảng Ngãi”.
+
+select dich_vu_di_kem.ma_dvdk, dich_vu_di_kem.ten_dvdk, dich_vu_di_kem.gia from dich_vu_di_kem inner join hop_dong_chi_tiet on hop_dong_chi_tiet.ma_dvdk = dich_vu_di_kem.ma_dvdk 
+inner join hop_dong on hop_dong_chi_tiet.ma_hd = hop_dong.ma_hd inner join khach_hang on khach_hang.ma_kh = hop_dong.ma_kh 
+inner join loai_khach on loai_khach.ma_lk = khach_hang.ma_lk where ten_lk = "Diamond" 
+and (dia_chi_kh like "%, Vinh" or dia_chi_kh like "%, Quảng Ngãi");
+
+-- 12.	Hiển thị thông tin ma_hop_dong, ho_ten (nhân viên), ho_ten (khách hàng), so_dien_thoai (khách hàng), ten_dich_vu,
+-- so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem), 
+-- tien_dat_coc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2020 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2021
+
